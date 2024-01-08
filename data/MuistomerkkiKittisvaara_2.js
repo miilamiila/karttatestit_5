aba384f7043a82d4bf1e5be5e36234d778ba9d79
miilamiila <153753331+miilamiila@users.noreply.github.com>
2024-01-08 10:07:57 +0200

    Add files via upload

---
+++ data/MuistomerkkiKittisvaara_2.js
@@ -0,0 +1,1 @@
+var json_MuistomerkkiKittisvaara_2 = {"type":"FeatureCollection","name":"MuistomerkkiKittisvaara_2","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":"2"},"geometry":{"type":"Point","coordinates":[24.007843514103055,66.809468677350054]}}]}
